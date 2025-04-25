1656e1d768bab7be61a111a3c1580fa3d5f32f16
knakatasf <knakata@dons.usfca.edu>
2025-04-24 20:48:57 -0700

    Added workflow

--- qa/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-tfstate-bucket"
-    key            = "qa/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "your-lock-table"
-  }
-}
