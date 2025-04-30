fc0634be424e3e739159cc914aa3b5c7a765bf6c
knakatasf <knakata@dons.usfca.edu>
2025-04-30 10:54:36 -0700

    Added workflow

--- qa/terraform/backend.tf
+++ qa/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-org-terraform-state"
+    bucket         = "my-org-terraform-state-1"
     key            = "qa/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
